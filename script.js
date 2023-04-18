bf3668d3f9fb5d5b0bceaa428b3228f8dea4ffae
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-18 08:48:34 +0300

    Move all the grid creating and coloring functionality into the createGrid function

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const container = document.querySelector('.container');
-for (let i=0; i<16; i++) {
-    const div = document.createElement('div');
-    div.className = 'row';
-    for (let j=0; j<16; j++) {
-        const childDiv = document.createElement('div');
-        childDiv.className='block';
-        div.appendChild(childDiv);
+
+function createGrid(size){
+    let teDelete = document.querySelectorAll('.row');
+    teDelete.forEach(row => row.remove());
+    for (let i=0; i<size; i++) {
+        const div = document.createElement('div');
+        div.className = 'row';
+        for (let j=0; j<size; j++) {
+            const childDiv = document.createElement('div');
+            childDiv.className='block';
+            div.appendChild(childDiv);
+        }
+        container.appendChild(div);
     }
-    container.appendChild(div);
+
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => block.addEventListener('mouseenter', ()=>{
+        block.style.backgroundColor=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+    },
+    {
+        once:true
+    }));
 }
 
-const blocks = document.querySelectorAll('.block');
-blocks.forEach(block => block.addEventListener('mouseenter', ()=>{
-    block.style.backgroundColor=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
-},
-{
-    once:true
-}));
-console.log(blocks)
+createGrid(16);
+
