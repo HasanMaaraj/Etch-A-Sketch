29a13d1c4c90f8292855a36b8dd2c929384b3a61
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-18 08:21:12 +0300

    Add coloring functionality
    -Add class name 'block' to the grid divs
    -Add event listener that will color the block divs with random colors when the mouse enters

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ for (let i=0; i<16; i++) {
     div.className = 'row';
     for (let j=0; j<16; j++) {
         const childDiv = document.createElement('div');
+        childDiv.className='block';
         div.appendChild(childDiv);
     }
     container.appendChild(div);
 }
+
+const blocks = document.querySelectorAll('.block');
+blocks.forEach(block => block.addEventListener('mouseenter', ()=>{
+    block.style.backgroundColor=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+},
+{
+    once:true
+}));
+console.log(blocks)
