a4b98975110414139399f1f3931dea523433f5c4
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-18 06:57:24 +0300

    Edit the divs insertion to the html
    -Create a div in the html with the class name of container
    -Rename the class of the divs to row in the script
    -Append the divs that have been created in the script to the container div instead of the body

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const body = document.querySelector('body');
+const container = document.querySelector('.container');
 for (let i=0; i<16; i++) {
     const div = document.createElement('div');
-    div.className = 'container';
+    div.className = 'row';
     for (let j=0; j<16; j++) {
         const childDiv = document.createElement('div');
         div.appendChild(childDiv);
     }
-    body.appendChild(div);
+    container.appendChild(div);
 }
